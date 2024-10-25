e4f791a81112a8975786f73eddc71ed9ab5325de
Nicole Holloway <jessinthehallway@gmail.com>
2024-10-25 11:09:30 +0800

    clean: trailing tab

--- app.js
+++ app.js
@@ -19,55 +19,55 @@ load_commands()
 const fatalError = function(){
     console.log("A fatal error has occurred! The program could not auto-recover.")
 }
-    function commandLineMode(returned = false){
-        try{
+function commandLineMode(returned = false){
+    try{
 
-            if(returned == false){
-                console.log('\nTerminal Simulator VERSION 2. To see available commands, type "help".')
-            }
-            rl.question('> ', ans => {
-                //JSON check
-                try{
-                    JSON.parse(fs.readFileSync('./resources/commands.json'))
-                }catch{
-                    return fatalError()
-                }
-
-                if(require.cache[require.resolve('./resources/commands.json')]){
-                    delete require.cache[require.resolve('./resources/commands.json')]
-                }
-                var availableCommands = require('./resources/commands.json')
-                let args = ans.split(/ +/);
-                if(availableCommands[args[0]]){
-                    if(ans.toLowerCase().startsWith("help")){
-                        let helpMenu = []
-                        helpMenu.push('==========================================')
-                        helpMenu.push('AVAILABLE COMMANDS\n')
-                        for (let index = 0; index < Object.keys(availableCommands).length; index++) {
-                            const element = Object.keys(availableCommands)[index];
-                            helpMenu.push(element +'    '+availableCommands[Object.keys(availableCommands)[index]].about)
-                        }
-                        helpMenu.push('==========================================')
-                        helpMenu.forEach(element => {
-                            console.log(element)
-                        });
-                    }else
-                    if(ans.toLowerCase().startsWith("exit")){
-                        console.log("Exiting...\n")
-                        return process.exit()
-                    }else{
-                        eval(availableCommands[args[0]].code)
-                    }
-                    return commandLineMode(true)
-                }else{
-                    if(args[0] !=''){
-                        console.log(`"${args[0]}" is not a valid command.`)
-                    }
-                    return commandLineMode(true)
-                }
-            })
-        }catch{
-            return fatalError()
+        if(returned == false){
+            console.log('\nTerminal Simulator VERSION 2. To see available commands, type "help".')
         }
+        rl.question('> ', ans => {
+            //JSON check
+            try{
+                JSON.parse(fs.readFileSync('./resources/commands.json'))
+            }catch{
+                return fatalError()
+            }
+
+            if(require.cache[require.resolve('./resources/commands.json')]){
+                delete require.cache[require.resolve('./resources/commands.json')]
+            }
+            var availableCommands = require('./resources/commands.json')
+            let args = ans.split(/ +/);
+            if(availableCommands[args[0]]){
+                if(ans.toLowerCase().startsWith("help")){
+                    let helpMenu = []
+                    helpMenu.push('==========================================')
+                    helpMenu.push('AVAILABLE COMMANDS\n')
+                    for (let index = 0; index < Object.keys(availableCommands).length; index++) {
+                        const element = Object.keys(availableCommands)[index];
+                        helpMenu.push(element +'    '+availableCommands[Object.keys(availableCommands)[index]].about)
+                    }
+                    helpMenu.push('==========================================')
+                    helpMenu.forEach(element => {
+                        console.log(element)
+                    });
+                }else
+                if(ans.toLowerCase().startsWith("exit")){
+                    console.log("Exiting...\n")
+                    return process.exit()
+                }else{
+                    eval(availableCommands[args[0]].code)
+                }
+                return commandLineMode(true)
+            }else{
+                if(args[0] !=''){
+                    console.log(`"${args[0]}" is not a valid command.`)
+                }
+                return commandLineMode(true)
+            }
+        })
+    }catch{
+        return fatalError()
     }
-    commandLineMode()
+}
+commandLineMode()
