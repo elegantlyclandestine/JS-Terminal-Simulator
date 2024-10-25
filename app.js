0cbb917a97ec865c154cbf59cba522140d9092e0
Nicole Holloway <jessinthehallway@gmail.com>
2024-10-25 11:32:09 +0800

    incl: fatalError and command processor

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const fs = require('fs');
 const readline = require("readline");
 const { load_commands } = require('./modules/command_handler.js');
-const { terminal } = require('./modules/terminal.js');
+const { terminal, fatalError } = require('./modules/terminal.js');
+const { process_command } = require('./commands/__command_directory__.js');
 const rl = readline.createInterface({input: process.stdin,output: process.stdout});
 fetch = require('node-fetch');
 const connect = async (url) => {
