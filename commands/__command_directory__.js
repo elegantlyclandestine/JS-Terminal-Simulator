8058268e6f3d41b4dc730b6e03027380df910fd9
Nicole Holloway <jessinthehallway@gmail.com>
2024-10-25 11:28:27 +0800

    def: command enum + command processor

---
+++ commands/__command_directory__.js
@@ -0,0 +1,19 @@
+const { help } = require("help.js")
+
+const cmds = Object.freeze ({
+    HELP: 0,
+    EXIT: 1 
+})
+
+function process_command(command) {
+    switch (command) {
+        case cmds.HELP:
+            help()
+            break;
+        case cmds.EXIT:
+            process.exit()
+            break;
+        default:
+            console.log("Command not found!")
+    }
+}
